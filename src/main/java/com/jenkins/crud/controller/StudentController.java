e722c8eb1a34f041ad365250f6a60b776f1214c5
Sachin Mallabade <sachinmallabade@gmail.com>
2024-10-15 15:49:22 +0530

    get student by id implemented

--- src/main/java/com/jenkins/crud/controller/StudentController.java
+++ src/main/java/com/jenkins/crud/controller/StudentController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,13 @@ public class StudentController {
 	public List<Student> getStudent()
 	{
 		return studentRepository.findAll();
+	}
+	
+	@GetMapping("/student/{id}")
+	public Student getStudentById(@PathVariable int id)
+	{
+		Student student = studentRepository.findById(id).get();
+		return student;
 	}
 	
 	@PostMapping("/student/add")
